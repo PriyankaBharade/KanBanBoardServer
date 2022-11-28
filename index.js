1374bbd4efa8218932b9cfdfc494495c6b6dbdae
PB62779 <priyanka.bharade7apr@gmail.com>
2022-11-28 10:44:25 +0530

    update changes in index

--- index.js
+++ index.js
@@ -1,128 +1,127 @@
-// const express = require("express");
-// const app = express();
-// const PORT = 4000;
+const express = require("express");
+const app = express();
+const PORT = 4000;
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// const http = require("http").Server(app);
-// const cors = require("cors");
-// app.use(cors());
+const http = require("http").Server(app);
+const cors = require("cors");
+app.use(cors());
 
-// const socketIO = require('socket.io')(http, {
-// 	cors: {
-// 		origin: "http://localhost:3000"
-// 	}
-// });
+const socketIO = require('socket.io')(http, {
+	cors: {
+		origin: "http://localhost:3000"
+	}
+});
 
-// socketIO.on('connection', (socket) => {
-// 	console.log(`⚡: ${socket.id} user just connected!`);
-// 	socket.on('disconnect', () => {
-// 		socket.disconnect()
-// 		console.log('🔥: A user disconnected');
-// 	});
-// 	socket.on("taskDragged", (data) => {
-// 		const { source, destination } = data;
+socketIO.on('connection', (socket) => {
+	console.log(`⚡: ${socket.id} user just connected!`);
+	socket.on('disconnect', () => {
+		socket.disconnect()
+		console.log('🔥: A user disconnected');
+	});
+	socket.on("taskDragged", (data) => {
+		const { source, destination } = data;
 
-// 		const itemMoved = {
-// 			...tasks[source.droppableId].items[source.index],
-// 		};
-// 		console.log("DraggedItem>>> ", itemMoved);
+		const itemMoved = {
+			...tasks[source.droppableId].items[source.index],
+		};
+		console.log("DraggedItem>>> ", itemMoved);
 
-// 		tasks[source.droppableId].items.splice(source.index, 1);
+		tasks[source.droppableId].items.splice(source.index, 1);
 
-// 		tasks[destination.droppableId].items.splice(destination.index, 0, itemMoved);
+		tasks[destination.droppableId].items.splice(destination.index, 0, itemMoved);
 
-// 		socket.emit("tasks", tasks);
-// 	});
-// });
+		socket.emit("tasks", tasks);
+	});
+});
 
-// app.listen(PORT, () => {
-// 	console.log(`Server listening on ${PORT}`);
-// });
+app.listen(PORT, () => {
+	console.log(`Server listening on ${PORT}`);
+});
 
 
-//2nd option
+// //2nd option
 
-const express = require("express")
-const app = express()
-const http = require("http")
-const cors = require("cors")
-const { Server } = require("socket.io")
-app.use(cors)
-const server = http.createServer(app)
-const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-    }
-})
-io.on("connection", (socket) => {
-    console.log("Socket Id", socket.id)
+// const express = require("express")
+// const app = express()
+// const http = require("http")
+// const cors = require("cors")
+// const { Server } = require("socket.io")
+// app.use(cors)
+// const server = http.createServer(app)
+// const io = new Server(server, {
+//     cors: {
+//         origin: "http://localhost:3000",
+//         methods: ["GET", "POST"],
+//     }
+// })
+// io.on("connection", (socket) => {
+//     console.log("Socket Id", socket.id)
 
-    socket.on("disconnect", () => {
-        console.log("User Disconnected", socket.id)
-    })
+//     socket.on("disconnect", () => {
+//         console.log("User Disconnected", socket.id)
+//     })
 
-    //socket.emit("getTask", tasks);
+//     //socket.emit("getTask", tasks);
 
-    socket.on("createTask", (data) => {
-        const newTask = { id: fetchID(), title: data.task, comments: [] };
-        var item = []
-        // item.push(newTask)
-        tasks["pending"].items.push(newTask);
-        // tasks = {
-        //     "pending" : {
-        //         "title" : 'pending',
-        //         "items" : item
-        //       }
-        // }
-        socket.emit("tasks", tasks);
-        console.log("createTask>>> ", tasks);
-        // 👇🏻 sends notification via Novu
-        // sendNotification(data.userId);
-    });
+//     socket.on("createTask", (data) => {
+//         const newTask = { id: fetchID(), title: data.task, comments: [] };
+//         var item = []
+//         // item.push(newTask)
+//         tasks["pending"].items.push(newTask);
+//         // tasks = {
+//         //     "pending" : {
+//         //         "title" : 'pending',
+//         //         "items" : item
+//         //       }
+//         // }
+//         socket.emit("tasks", tasks);
+//         console.log("createTask>>> ", tasks);
+//         // 👇🏻 sends notification via Novu
+//         // sendNotification(data.userId);
+//     });
 
-    socket.on("fetchComments", (data) => {
-        const taskItems = tasks[data.category].items;
-        for (let i = 0; i < taskItems.length; i++) {
-            if (taskItems[i].id === data.id) {
-                socket.emit("comments", taskItems[i].comments);
-            }
-        }
-    });
-    socket.on("addComment", (data) => {
-        console.log("Add Comment", data)
-        const taskItems = tasks[data.category].items;
-        for (let i = 0; i < taskItems.length; i++) {
-            if (taskItems[i].id === data.id) {
-                taskItems[i].comments.push({
-                    name: data.userId,
-                    text: data.comment,
-                    id: fetchID(),
-                });
-                socket.emit("comments", taskItems[i].comments);
-            }
-        }
-    });
-    socket.on("taskDragged", (data) => {
-        const { source, destination } = data;
+//     socket.on("fetchComments", (data) => {
+//         const taskItems = tasks[data.category].items;
+//         for (let i = 0; i < taskItems.length; i++) {
+//             if (taskItems[i].id === data.id) {
+//                 socket.emit("comments", taskItems[i].comments);
+//             }
+//         }
+//     });
+//     socket.on("addComment", (data) => {
+//         console.log("Add Comment", data)
+//         const taskItems = tasks[data.category].items;
+//         for (let i = 0; i < taskItems.length; i++) {
+//             if (taskItems[i].id === data.id) {
+//                 taskItems[i].comments.push({
+//                     name: data.userId,
+//                     text: data.comment,
+//                     id: fetchID(),
+//                 });
+//                 socket.emit("comments", taskItems[i].comments);
+//             }
+//         }
+//     });
+//     socket.on("taskDragged", (data) => {
+//         const { source, destination } = data;
 
-        const itemMoved = {
-            ...tasks[source.droppableId].items[source.index],
-        };
-        tasks[source.droppableId].items.splice(source.index, 1);
+//         const itemMoved = {
+//             ...tasks[source.droppableId].items[source.index],
+//         };
+//         tasks[source.droppableId].items.splice(source.index, 1);
 
-        tasks[destination.droppableId].items.splice(destination.index, 0, itemMoved);
+//         tasks[destination.droppableId].items.splice(destination.index, 0, itemMoved);
 
-        socket.emit("tasks", tasks);
-    });
-})
-server.listen(process.env.PORT || 4000, () => {
-    console.log("SERVER RUNNING")
-})
+//         socket.emit("tasks", tasks);
+//     });
+// })
+// server.listen(process.env.PORT || 4000, () => {
+//     console.log("SERVER RUNNING")
+// })
 
-app.set(server)
 app.get("/api", (req, res) => {
     res.json(tasks);
 });
